8f46be80208b147c16efe999f2cb0c3f6ad0e6e0
nabin <nkbhaie2017@gmail.com>
2023-02-17 14:07:17 -0700

    add all java files

---
+++ FirstJava/src/com/BasicJava/Main.java
@@ -0,0 +1,9 @@
+package com.BasicJava;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hellow Developers out there");
+    }
+}
